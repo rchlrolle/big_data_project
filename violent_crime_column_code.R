b6c368e09e6aec6fe64579c6070b62e4d6dac57e
S-DeFerrari <66847874+S-DeFerrari@users.noreply.github.com>
2024-01-09 15:57:07 -0500

    Delete violent_crime_column_code.R

--- violent_crime_column_code.R
+++
@@ -1,7 +0,0 @@
-# Need to create our list of violent crimes per DC
-viol_crime <- c("HOMICIDE", "SEX ABUSE", "ROBBERY", "ASSAULT W/DANGEROUS WEAPON")
-
-# create our violent_crime column and reorder the columns so its first followed by offense
-Crime_Incidents_in_2023 = Crime_Incidents_in_2023 %>% 
-  mutate(VIOLENT_CRIME = ifelse(OFFENSE %in% viol_crime, 1, 0)) %>% 
-  select(VIOLENT_CRIME, OFFENSE, everything())
